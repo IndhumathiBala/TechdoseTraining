83753e15f457f122b929092a49d38b45586da674
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-09-03 11:51:13 +0530

    Update changeCase.java

--- Day3/HomeWork/changeCase.java
+++ Day3/HomeWork/changeCase.java
@@ -4,11 +4,11 @@ public class Main
     public static void main(String[] args) {
            Scanner sc=new Scanner(System.in);
            char ch=sc.next().charAt(0);
-        //   IN LOWERCASE
+        //   IN LOWERCASE--set 6th bit if it is not set(|)
            System.out.println((char)(ch|(1<<5)));
-        //   IN UPPERCASE
+        //   IN UPPERCASE--unset 6th bit if it is set(&)
            System.out.println((char)(ch&(~(1<<5))));
-        //   FLIPCASE
+        //   FLIPCASE--TOGGLE 6 th bit
            System.out.println((char)(ch^(1<<5)));
 	}
 }
