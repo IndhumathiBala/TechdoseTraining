431f5e20d02847486b0d0a32c2638a23f0d0307e
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-09-09 14:12:57 +0530

    Create lcm.java

---
+++ Basic_Problems/Day1/lcm.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main
+{
+    static int findGcd(int a,int b)
+    {
+        return b==0?a:findGcd(b,a%b);
+    }
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int a=sc.nextInt(),b=sc.nextInt();
+	    int gcd=findGcd(a,b);
+	    System.out.println((a*b)/gcd);
+	}
+}
