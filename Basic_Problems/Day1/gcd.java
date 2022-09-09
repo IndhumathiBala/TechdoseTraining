08da9a3fb5abbdc001da341a3764e740e22b6369
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-09-09 14:08:18 +0530

    Update gcd.java

--- Basic_Problems/Day1/gcd.java
+++ Basic_Problems/Day1/gcd.java
@@ -1,14 +1,15 @@
 import java.util.Scanner;
 public class Main
 {
-    int findGcd(int a,int b)
+    static int findGcd(int a,int b)
     {
-        return b==0?a:gcd(b,a%b);
+        return b==0?a:findGcd(b,a%b);
     }
 	public static void main(String[] args) {
 	    Scanner sc=new Scanner(System.in);
 	    int a=sc.nextInt();
 	    int b=sc.nextInt();
 	    int gcd=findGcd(a,b);
+	    System.out.println(gcd);
 	}
 }
