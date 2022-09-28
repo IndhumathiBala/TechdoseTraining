b60d2107f5786f2e41cb9473f882e23024658040
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-09-28 18:43:21 +0530

    Create 7.Rotate Image.java

---
+++ Arrays/Assignments/7.Rotate Image.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void rotate(int[][] mat) {
+        int n=mat.length;
+        for(int i=0;i<n/2;i++)
+        {
+            for(int j=i;j<n-i-1;j++)
+            {
+                int temp=mat[i][j];
+                mat[i][j]=mat[n-1-j][i];
+                mat[n-1-j][i]=mat[n-i-1][n-j-1];
+                mat[n-i-1][n-1-j]=mat[j][n-1-i];
+                mat[j][n-1-i]=temp;
+            }
+        }
+    }
+}
