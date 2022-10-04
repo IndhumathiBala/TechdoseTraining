561574188ca949442dd36bd6f88f67d51eb0fe36
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-10-04 21:20:58 +0530

    Create 018.Four Sum.java

---
+++ Assignment_Sheet_Problems/018.Four Sum.java
@@ -0,0 +1,39 @@
+
+// TC:O(n^3)    SC:O(1)
+class Solution {
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        int n=nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>>res=new ArrayList<>();
+        for(int i=0;i<n-3;i++)
+        {
+            for(int j=i+1;j<n-2;j++)
+            {
+                long tar=(long)target-(nums[i]+nums[j]);
+                int l=j+1,h=n-1;
+                while(l<h)
+                {
+                    if(nums[l]+nums[h]==tar)
+                    {
+                       res.add(Arrays.asList(nums[i],nums[j],nums[l],nums[h])); 
+                       while(l<h && nums[l]==nums[l+1])l++;
+                       while(l<h && nums[h]==nums[h-1])h--;
+                       l++;h--;
+                    }
+                    else if(nums[l]+nums[h]<tar)
+                    {
+                        l++;
+                    }
+                    else
+                    {
+                        h--;
+                    }
+                }
+                while(j<n-3 && nums[j]==nums[j+1])j++;
+            }
+            while(i<n-4 && nums[i]==nums[i+1])i++;
+        }
+        return res;
+        
+    }
+}
