08ab3d4f06a176b2e7c180938adb92886b61269f
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-10-06 15:07:16 +0530

    Update Subarray sum equals K.java

--- Arrays/HomeWork_2/Subarray sum equals K.java
+++ Arrays/HomeWork_2/Subarray sum equals K.java
@@ -3,10 +3,13 @@ class Solution {
         int n=nums.length;
         int sum=0,count=0;
         Map<Integer,Integer>map=new HashMap<>();
-        map.put(0,1);
         for(int i=0;i<n;i++)
         {
             sum+=nums[i];
+            if(sum==k)
+            {
+                count++;
+            }
             if(map.containsKey(sum-k))
             {
                 count+=map.get(sum-k);
