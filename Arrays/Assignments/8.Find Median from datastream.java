dcc3aa67fad527a05671742b5811807fb0b33318
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-10-10 14:58:41 +0530

    Create 8.Find Median from datastream.java

---
+++ Arrays/Assignments/8.Find Median from datastream.java
@@ -0,0 +1,25 @@
+class MedianFinder {
+    PriorityQueue<Integer>min=new PriorityQueue<>();
+    PriorityQueue<Integer>max=new PriorityQueue<>(Collections.reverseOrder());
+    public MedianFinder() {
+        
+    }
+    
+    public void addNum(int num) {
+        max.add(num);
+        min.add(max.poll());
+        if(min.size()>max.size())
+        {
+            max.add(min.poll());
+        }
+        
+    }
+    
+    public double findMedian() {
+        if(max.size()>min.size())
+            return max.peek();
+        else
+            return ((double)(max.peek()+min.peek()))/2.0;
+        
+    }
+}
