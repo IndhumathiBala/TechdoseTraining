c2c3bdf3b78a89e8fb7fa43e3f29b5b9c77b0f50
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-10-14 12:27:15 +0530

    Create 009.Find closest node to two given nodes.java

---
+++ Contest_Problems/3rd_Problem/009.Find closest node to two given nodes.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int closestMeetingNode(int[] edges, int node1, int node2) 
+    {
+        int n=edges.length,ans=edges.length,max=-1,closest=-1;
+        boolean []visited=new boolean[n];  
+        int []dist_1=new int[n];
+        int []dist_2=new int[n];
+        Arrays.fill(dist_1,-1);
+        Arrays.fill(dist_2,-1);
+        dfs(node1,edges,dist_1,visited,0);
+        visited=new boolean[n];
+        dfs(node2,edges,dist_2,visited,0);
+        for(int i=0;i<n;i++)
+        {
+            if(dist_1[i]!=-1 && dist_2[i]!=-1)
+            {
+                if(Math.max(dist_1[i],dist_2[i])<ans)
+                {
+                ans=Math.max(dist_1[i],dist_2[i]);
+                closest=i;
+                }
+            }
+        }
+        return closest;
+        
+    }
+    void dfs(int node,int edges[],int []dist,boolean[]visited,int d)
+    {
+        if(node!=-1 && !visited[node] )
+        {
+            visited[node]=true;
+            dist[node]=d;
+            dfs(edges[node],edges,dist,visited,d+1);
+        }
+    }
+}
