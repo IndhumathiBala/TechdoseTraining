c838bff5528490ca8d77525f7c540de168a95635
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-10-30 20:35:38 +0530

    Update 026.LRU cache.java

--- Assignment_Sheet_Problems/026.LRU cache.java
+++ Assignment_Sheet_Problems/026.LRU cache.java
@@ -1,56 +1,76 @@
 class LRUCache {
     class Node
-{
-    int key,value;
-    Node(int k,int v)
     {
-        this.key=k;
-        this.value=v;
+        Node prev,next;
+        int key,val;
+        Node(int k,int v)
+        {
+            this.key=k;
+            this.val=v;
+        }
+        Node(){}
     }
-}
-    int capacity;
-    Map<Integer,Node>map=new HashMap<>();
+    int capacity,count;
+    Map<Integer,Node>map;
+    Node head,tail;
     Deque<Node>q=new LinkedList<>();
     public LRUCache(int capacity) {
         this.capacity=capacity;
-        
+        count=0;
+        map=new HashMap<>();
+        head=new Node();
+        tail=new Node();
+        head.next=tail;
+        tail.prev=head;
     }
     
     public int get(int key) {
-        if(map.get(key)==null)
-        {
-            return -1;
-        }
         Node temp=map.get(key);
-        q.remove(temp);
-        q.addFirst(temp);
-        return map.get(key).value;
+        if(temp==null)
+            return -1;
+        else
+        {
+            remove(temp);
+            add(temp);
+            return temp.val;
+        }
         
     }
     
     public void put(int key, int value) {
-        if(map.get(key)!=null)
+        Node temp=map.get(key);
+        if(temp==null)
         {
-            Node temp=map.get(key);
-            temp.value=value;
-            q.remove(temp);
-            q.addFirst(temp);
+            Node nn=new Node(key,value);
+            map.put(key,nn);
+            add(nn);
+            count++;
         }
         else
         {
-            
-            if(q.size()==capacity)
-            {
-                Node node=q.removeLast();
-                map.remove(node.key);
-                
-            }
-            // System.out.println(q.size());
-            Node node=new Node(key,value);
-            map.put(key,node);
-            q.addFirst(node);
+            temp.val=value;
+            remove(temp);
+            add(temp);
         }
-        
+        if(count>capacity)
+        {
+            Node del=tail.prev;
+            remove(del);
+            map.remove(del.key);
+            count--;
+        }
+    }
+    public void add(Node nn)
+    {
+        nn.next=head.next;
+        nn.prev=head;
+        head.next=nn;
+        nn.next.prev=nn;
+    }
+    public void remove(Node nn)
+    {
+        nn.prev.next=nn.next;
+        nn.next.prev=nn.prev;
     }
 }
 
