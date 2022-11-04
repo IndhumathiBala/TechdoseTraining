4b8afaaacf9c51101a2d988af24780739d7a59d3
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-11-04 10:35:02 +0530

    Create Fibonacci(dynamic programming).java

---
+++ Recursion/Fibonacci(dynamic programming).java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    System.out.println(fibonacci(n));
+	}
+	static long fibonacci(int n)
+	{
+	    long []dp=new long[n+1];
+	    dp[0]=0;
+	    dp[1]=1;
+	    for(int i=2;i<=n;i++)
+	    {
+	        dp[i]=dp[i-1]+dp[i-2];
+	    }
+	    return dp[n];
+	}
+}
