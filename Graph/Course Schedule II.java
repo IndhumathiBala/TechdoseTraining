dbeddadde7b98e0b93f6a5b1b5bee0c5caf3a7c2
Indhumathi_B <88466628+IndhumathiBala@users.noreply.github.com>
2022-12-28 10:25:38 +0530

    Create Course Schedule II.java

---
+++ Graph/Course Schedule II.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int[] findOrder(int V, int[][] p) {
+        int n=p.length;
+          List<List<Integer>>adj=new ArrayList<>();
+          for(int i=0;i<V;i++)
+          {
+              adj.add(new ArrayList<>());
+          }
+          for(int i=0;i<n;i++)
+          {
+              adj.get(p[i][1]).add(p[i][0]);
+          }
+          List<Integer>res=topo(adj,n,V);
+          if(res.size()==V)
+          return res.stream().mapToInt(k->k).toArray();
+            return new int[]{};
+    }
+    List<Integer>topo(List<List<Integer>>adj,int n,int V)
+    {
+        int []indegree=new int[V];
+        for(int i=0;i<V;i++)
+        {
+            for(int j:adj.get(i))
+            {
+              indegree[j]++;  
+            }
+        }
+        Queue<Integer>q=new LinkedList<>();
+        for(int i=0;i<V;i++)
+        {
+            if(indegree[i]==0)
+            {
+                q.add(i);
+            }
+        }
+        List<Integer>res=new ArrayList<>();
+        int ind=0;
+        while(!q.isEmpty())
+        {
+            int parent=q.poll();
+            res.add(parent);
+            for(int it:adj.get(parent))
+            {
+                indegree[it]--;
+                if(indegree[it]==0)
+                {
+                    q.add(it);
+                }
+            }
+        }
+        return res;
+    }
+}
